8d7cfa2f2a4a43c5490cff80458c8d0c41199872
frank906 <ceccoangiolieri1@libero.it>
2019-11-12 00:31:13 +0100

    Update server.R

--- server.R
+++ server.R
@@ -3,17 +3,65 @@ library(shiny)
 # Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
 
-  # Expression that generates a plot of the distribution. The expression
-  # is wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically 
-  #     re-executed when inputs change
-  #  2) Its output type is a plot 
-  #
-  output$distPlot <- renderPlot({
-
-    # generate an rnorm distribution and plot it
-    dist <- rnorm(input$obs)
-    hist(dist)
+  # Define server logic to read selected file ----
+  options(shiny.maxRequestSize=1000*1024^2)
+  eventlog <- reactive({
+    
+    
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    
+    
+    data <- readr::read_csv(input$file1$datapath)
+    # load BPI Challenge 2017 data set ####
+    
+    
+    # change timestamp to date var
+    data$starttimestamp = as.POSIXct(data$'Start Timestamp', 
+                                     format = "%Y/%m/%d %H:%M:%S")
+    
+    data$endtimestamp = as.POSIXct(data$'Complete Timestamp', 
+                                   format = "%Y/%m/%d %H:%M:%S")
+    
+    
+    
+    
+    
+    
+    
+    eventlog <- bupaR::activities_to_eventlog(
+      data,
+      case_id = 'Case_ID',
+      activity_id = 'Activity',
+      resource_id = 'Resource',
+      timestamps = c('starttimestamp', 'endtimestamp')
+    )
   })
+  
+  
+  
+  
+  # process_map ---------------------------------------------------------------
+  output$process_map <- renderUI({
+    
+    tagList(
+      
+      
+      renderGrViz({
+        
+        eventlog() %>%
+          processmapR::process_map()
+        
+        
+        
+      })
+    )
+  })
+  
+  
+  
+
 })
