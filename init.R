17ff3094d66d479a06bc12e3442bf05d34be1884
frank906 <ceccoangiolieri1@libero.it>
2019-11-12 08:52:52 +0100

    Update and rename packages.R to init.R

---
+++ init.R
@@ -0,0 +1,21 @@
+my_packages = c(
+  'bupaR',
+  'edeaR',
+  'processmapR',
+  'eventdataR',
+  'readr',
+  'tidyverse',
+  'DiagrammeR',
+  'ggplot2',
+  'stringr',
+  'lubridate'  
+)
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
